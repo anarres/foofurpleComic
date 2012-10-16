684847cbc101d5cf868485942614515e8ec19c2b
foo <katie@foo.(none)>
2012-10-16 14:05:37 +0100

    moar awwrsome!

---
+++ comicBubbles.js
@@ -0,0 +1,29 @@
+var bubbleUrls = [
+    [
+        ["images/bubbles/leftBubble1.png", "images/bubbles/rightBubble1.png"],
+        ["images/bubbles/leftBubble2.png", "images/bubbles/rightBubble2.png"],
+        ["images/bubbles/leftBubble3.png", "images/bubbles/rightBubble3.png"],
+
+        ["images/bubbles/leftBubble4.png", "images/bubbles/rightBubble4.png"],
+        ["images/bubbles/leftBubble5.png", "images/bubbles/rightBubble5.png"],
+        ["images/bubbles/leftBubble6.png", "images/bubbles/rightBubble6.png"],
+
+        ["images/bubbles/leftBubble7.png", "images/bubbles/rightBubble7.png"],
+        ["images/bubbles/leftBubble8.png", "images/bubbles/rightBubble8.png"],
+        ["images/bubbles/leftBubble9.png", "images/bubbles/rightBubble9.png"]
+    ],
+    [
+        ["images/bubbles/leftCloud1.png", "images/bubbles/rightCloud1.png"],
+        ["images/bubbles/leftCloud2.png", "images/bubbles/rightCloud2.png"],
+        ["images/bubbles/leftCloud3.png", "images/bubbles/rightCloud3.png"],
+
+        ["images/bubbles/leftCloud4.png", "images/bubbles/rightCloud4.png"],
+        ["images/bubbles/leftCloud5.png", "images/bubbles/rightCloud5.png"],
+        ["images/bubbles/leftCloud6.png", "images/bubbles/rightCloud6.png"],
+
+        ["images/bubbles/leftCloud7.png", "images/bubbles/rightCloud7.png"],
+        ["images/bubbles/leftCloud8.png", "images/bubbles/rightCloud8.png"],
+        ["images/bubbles/leftCloud9.png", "images/bubbles/rightCloud9.png"]
+    ]
+];
+
